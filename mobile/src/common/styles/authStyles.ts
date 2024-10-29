f80bcc9a151f8275dc6945374dc4c68911748a48
Valia5 <valiashak5@gmail.com>
2024-10-29 20:14:02 +0200

    changed styles

--- mobile/src/common/styles/authStyles.ts
+++ mobile/src/common/styles/authStyles.ts
@@ -3,24 +3,22 @@ import { StyleSheet } from "react-native";
 export const authStyles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 41,
-    justifyContent: "center",
     backgroundColor: "#030E38",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
   },
-  headerOne: {
+
+  headerOne: { //CloudyWay
     fontSize: 55,
     fontWeight: "bold",
-    textAlign: "center",
+    color: "#fff",
     marginBottom: 40,
-    color: "#FAFAFA",
   },
-  headerTwo: {
-    fontSize: 26,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 24,
-    color: "#FFFFFF",
-    fontFamily: "Roboto",
+  headerTwo: { //sign/in/up
+      color: "#fff",
+      fontSize: 24,
+      marginBottom: 30,
   },
   commonText: {
     fontSize: 16,
@@ -30,13 +28,31 @@ export const authStyles = StyleSheet.create({
     color: "#FAFAFA",
   },
 
-  input: {
-    height: 40,
-    borderColor: "#0000001A",
-    backgroundColor: "#FAFAFA",
+  label: { //текст над полями
+    color: '#fff',
+    alignSelf: 'flex-start',
+    marginBottom: 5,
+  },
+
+  input: { //поля вводу
+    height: 50,
+    backgroundColor: '#fff',
+    borderColor: '#ccc',
     borderWidth: 1,
-    marginBottom: 16,
-    paddingLeft: 8,
-    borderRadius: 5,
+    borderRadius: 9,
+    paddingHorizontal: 10,
+  },
+  signInButton: {
+    backgroundColor: "#1E6AFF",
+    width: "90%",
+    paddingVertical: 12,
+    borderRadius: 8,
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  buttonText: { //для синіх полів
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
