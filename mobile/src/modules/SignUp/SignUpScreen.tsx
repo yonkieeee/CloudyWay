ea6c2b63a4bf6ff74827db3d6c015c65a429f376
Yulia <yuliaduma25@gmail.com>
2024-11-13 19:16:18 +0200

    Зберегти локальні зміни перед злиттям

--- mobile/src/modules/SignUp/SignUpScreen.tsx
+++ mobile/src/modules/SignUp/SignUpScreen.tsx
@@ -1,15 +1,93 @@
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, View, TextInput, Alert, TouchableOpacity } from "react-native";
+import authStyles from "@/src/common/styles/authStyles";
 
-export default function SignUpScreen() {
+const SignUpScreen: React.FC = () => {
+  const [username, setUsername] = useState<string>(""); // Define username here
+  const [email, setEmail] = useState<string>("");
+  const [createPassword, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+
+  const handleSignUp = () => {
+    const obj = {
+      username: username,
+      email: email,
+      createPassword: createPassword,
+      confirmPassword: confirmPassword,
+    };
+    console.log("Lox", obj);
+
+    if (username && email && createPassword) {
+      Alert.alert("Success", "Account created successfully!");
+      // Можна додати логіку реєстрації, наприклад, запит до бекенду
+    } else {
+      Alert.alert("Error", "Please fill all the fields");
+    }
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>SignUp</Text>
+    <View style={authStyles.container}>
+      <Text style={authStyles.headerOne}>CloudyWay</Text>
+      <Text style={authStyles.headerTwo}>Create your account</Text>
+
+      <View style={authStyles.inputContainer}>
+        <Text style={authStyles.label}>Username</Text>
+        <TextInput
+          style={authStyles.input}
+          placeholder="Enter your username"
+          placeholderTextColor="#aaa"
+          value={username}
+          onChangeText={setUsername}
+        />
+      </View>
+
+      <View style={authStyles.inputContainer}>
+        <Text style={authStyles.label}>Email</Text>
+        <TextInput
+          style={authStyles.input}
+          placeholder="Enter your email"
+          placeholderTextColor="#aaa"
+          keyboardType="email-address"
+          value={email}
+          onChangeText={setEmail}
+        />
+      </View>
+
+      <View style={authStyles.inputContainer}>
+        <Text style={authStyles.label}>Create password</Text>
+        <TextInput
+          style={authStyles.input}
+          placeholder="Create your password"
+          placeholderTextColor="#aaa"
+          secureTextEntry
+          value={createPassword}
+          onChangeText={setPassword}
+        />
+      </View>
+
+      <View style={authStyles.inputContainer}>
+        <Text style={authStyles.label}>Confirm password</Text>
+        <TextInput
+          style={authStyles.input}
+          placeholder="Confirm your password"
+          placeholderTextColor="#aaa"
+          secureTextEntry
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+        />
+      </View>
+
+      <TouchableOpacity style={authStyles.signInButton} onPress={handleSignUp}>
+        <Text style={authStyles.buttonText}>Sign up</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={authStyles.signUpText}>
+          Already have an account?{" "}
+          <Text style={authStyles.signUpLink}>Sign up.</Text>
+        </Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
+
+export default SignUpScreen;
